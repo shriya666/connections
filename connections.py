f991513de3e8b4690a96cb972776f5189159c8fb
shriya666 <shriyarajurkar3@gmail.com>
2025-12-16 02:30:12 +0000

    connectionsgit add .git add .

--- connections.py
+++ connections.py
@@ -1,10 +1,9 @@
 import random 
+import os
 def gencat(category_file):
     f=open("wordbanks/" + category_file)
     thefile=f.read()
-    #print(thefile)
     this_list=thefile.split("\n")
-    #print(this_list)
     category_yellow=[]
     for i in range(0, 4):
         the_word=random.randint(0, len(this_list) - 1)
@@ -12,38 +11,52 @@ def gencat(category_file):
         this_list.remove(this_actual_thing)
         category_yellow.append(this_actual_thing)
     return category_yellow
+yellow_random=random.choice(os.listdir("wordbanks/yellow"))
+green_random=random.choice(os.listdir("wordbanks/green"))
+blue_random=random.choice(os.listdir("wordbanks/blue"))
+purple_random=random.choice(os.listdir("wordbanks/purple"))
 
 
 def actual_game():
-    yellow_items = gencat("fruits")
-    green_items = gencat("disney movies")
-    your_guesses=[]
+    categories={
+        "yellow": gencat("yellow/"+yellow_random),
+        "green": gencat("green/"+green_random),
+        "blue": gencat("blue/"+blue_random), 
+        "purple":gencat("purple/"+purple_random)
+        }
+    correct_guesses={
+        "yellow": [],
+        "green" : [],
+        "blue": [],
+        "purple": []
+    }
+    s=[
+    *categories["yellow"],
+    *categories["green"],
+    *categories["blue"],
+    *categories["purple"],
+    ]
+    random.shuffle(s)
+    print(s)
     for i  in range(0, 4):
-        your_guess_word= input()
-        if your_guess_word in yellow_items:
-            print("i'")
-            your_guesses.append(your_guess_word)
+        your_guess_word= input().lower()
+        for color in categories:
+            if your_guess_word in categories[color]:
+                correct_guesses[color].append(your_guess_word)
+    for color in correct_guesses:
+        if len(correct_guesses[color])==4:
+            print("This means you did it for "+color)
+        elif len(correct_guesses[color])==3:
+            print("1 away for "+ color)
         else:
-            print("uhhh lets circle back")
-    print(your_guesses)
-    if len(your_guesses)==4:
-        print("This means you got it right. ")
-    elif len(your_guesses)==3:
-        print("One away")
-    else:
-        print("nope.")
-        
+            print("nope, for "+color)
+
         
     
 actual_game()
 
-def picking_words():
-    
 
 
 
 
-    #print(the_word)
-# number_of_words=thefile.count("\n")
-# new_list=[]
-# new_list.append()
+
